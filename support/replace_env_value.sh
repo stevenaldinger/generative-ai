dc5c4ba41642f54eeb295a9d75740a63e071dd17
Steven Aldinger <stevenaldinger@Stevens-MacBook-Pro.local>
2023-12-19 21:09:38 -0700

    fix: sed scripts on mac

--- support/replace_env_value.sh
+++ support/replace_env_value.sh
@@ -25,7 +25,11 @@ fi
 
 # if the env var is already set, replace it
 if grep -q $env_var_name $env_file_path; then
-  sed -i "s;${env_var_name}=.*;${env_var_name}=${env_var_value};g" $env_file_path
+  # can't use inline "sed -i" on MacOS because of VirtioFS
+  # https://forums.docker.com/t/sed-couldnt-open-temporary-file-xyz-permission-denied-when-using-virtiofs/125473
+  sed "s;${env_var_name}=.*;${env_var_name}=${env_var_value};g" $env_file_path > $env_file_path.tmp
+  
+  mv -f $env_file_path.tmp $env_file_path
 else
   # otherwise, add it to the end of the file
   echo "$env_var_name=$env_var_value" >> $env_file_path
